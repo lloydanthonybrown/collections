a8160087ce43d739262bbcaa6ad6ac2a598caf8c
lloydanthonybrown <lloydanthonybrown@gmail.com>
2015-11-29 19:20:02 -0700

    Finished the TreeSet class.

--- collectionsModule/src/Lists/ExampleArrayList.java
+++ collectionsModule/src/Lists/ExampleArrayList.java
@@ -15,52 +15,6 @@ public class ExampleArrayList {
         int anInteger;
         int count = 0;
         ArrayList<Integer> myIntegerArrayList = new ArrayList<Integer>();
-        ListIterator myListIterator = new ListIterator() {
-            @Override
-            public boolean hasNext() {
-                return false;
-            }
-
-            @Override
-            public Object next() {
-                return null;
-            }
-
-            @Override
-            public boolean hasPrevious() {
-                return false;
-            }
-
-            @Override
-            public Object previous() {
-                return null;
-            }
-
-            @Override
-            public int nextIndex() {
-                return 0;
-            }
-
-            @Override
-            public int previousIndex() {
-                return 0;
-            }
-
-            @Override
-            public void remove() {
-
-            }
-
-            @Override
-            public void set(Object o) {
-
-            }
-
-            @Override
-            public void add(Object o) {
-
-            }
-        };
 
         // Play with an integer ArrayList
         System.out.println("The current size of the array: " + myIntegerArrayList.size());
--- collectionsModule/src/Sets/ExampleTreeSet.java
+++ collectionsModule/src/Sets/ExampleTreeSet.java
@@ -24,9 +24,9 @@ public class ExampleTreeSet {
         System.out.println(alphabeticalTreeSet);
         System.out.println("Current size of the alphabeticalTreeSet: " + alphabeticalTreeSet.size());
         System.out.println("Returns a boolean for the alphabeticalTreeSet being empty or not: " + alphabeticalTreeSet.isEmpty());
+        System.out.println("Testing out the last() for alphabeticalTreeSet: " + alphabeticalTreeSet.first());
         System.out.println("Testing out the last() for alphabeticalTreeSet: " + alphabeticalTreeSet.last());
-        // use the addAll() with a different collection as a parameter.
-        alphabeticalTreeSet.addAll(numericalTreeSet);
+        alphabeticalTreeSet.comparator();
 
         System.out.println("**************************************");
         System.out.println("SECTION FOR numericalTreeSet");
@@ -43,13 +43,27 @@ public class ExampleTreeSet {
         System.out.println(numericalTreeSet);
         System.out.println("Current size of the numericalTreeSet: " + numericalTreeSet.size());
         System.out.println("Returns a boolean for the numericalTreeSet being empty or not: " + numericalTreeSet.isEmpty());
+        System.out.println("A subset of the numericalTreeSet: " + numericalTreeSet.subSet(73,90));
+        // Interesting to find that the subsets are exclusive by default.
+        System.out.println("A more inclusive subset of the numericalTreeSet: " + numericalTreeSet.subSet(73,true,90,true));
+        System.out.println("A tailset of the numericalTreeSet: " + numericalTreeSet.tailSet(73));
+        // This boolean is talking about the parameterized value (whether to include or exclude it).
+        System.out.println("A tailset and boolean of the numericalTreeSet: " + numericalTreeSet.tailSet(73,false));
 
         System.out.println("**************************************");
         System.out.println("SECTION FOR stringsTreeSet");
         System.out.println();
+        stringsTreeSet.add("Grapes");
+        stringsTreeSet.add("cherries");
+        stringsTreeSet.add("Apples");
+        stringsTreeSet.add("bananas");
+        stringsTreeSet.add("Watermelons");
+
         System.out.println(stringsTreeSet);
         System.out.println("Current size of the stringsTreeSet: " + stringsTreeSet.size());
         System.out.println("Returns a boolean for the stringsTreeSet being empty or not: " + stringsTreeSet.isEmpty());
+        stringsTreeSet.remove("Grapes");
+        System.out.println("After removing 'Grapes': " + stringsTreeSet);
 
         System.out.println("**************************************");
         System.out.println("SECTION FOR nasty paths");
@@ -70,8 +84,47 @@ public class ExampleTreeSet {
         }
 
         // Try to add different data types in the same TreeSet, like floats, doubles, integers and strings.
-        // Try entering negative or null values in a TreeSet.
+        try {
+            System.out.println("Try to add all the elements from the numericalTreeSet to the alphabetical one.");
+            alphabeticalTreeSet.addAll(numericalTreeSet);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Can't add different data types into a single collection.");
+        }
 
+        // Try entering null values in a TreeSet.
+        // Doesn't allow null values! I'm curious - why does it display my error messages all over the place every other
+        // time that I run this program? Are they being garbage collected in the mean time?
+        try {
+            stringsTreeSet.add(null);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Can't add a null value to this collection.");
+        }
+
+        // Try entering negative values in a TreeSet.
+        // Nothing happened. I guess it worked!
+        try {
+            numericalTreeSet.add(-14);
+            System.out.println(numericalTreeSet);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Can't add negative values to this collection.");
+        }
+
+        // Try removing something that doesn't exist.
+        // Nothing happens, so far as I can tell.
+        try {
+            stringsTreeSet.remove("grapes");
+            System.out.println(stringsTreeSet);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Can't remove an element that doesn't exist.");
+        }
 
     }
 }
